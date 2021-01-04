1b94dd28da6790febd2b75cf6957e03aa1c7cd5f
Stefan <stefan.vfl94@gmail.com>
2021-01-04 22:12:15 +0100

    mySQL databas av användarhantering
    Ändrat inloggningen så att användare kan skapas och sparas i en sql databas.

--- Game/View/LogInGUI.java
+++ Game/View/LogInGUI.java
@@ -5,6 +5,12 @@ import Game.Controller.Controller;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * This class contains the gui for the login menu
@@ -13,6 +19,10 @@ import java.awt.event.*;
  * @version 4.0
  */
 public class LogInGUI extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private String url = "jdbc:mysql://localhost:3306/sys2"; 
+    private String user = "Stefan";
+    private String password = "Ivana70a";
     private Controller controller;
     private ImageIcon welcomeLogo = new ImageIcon("images/MemoriaWelcome.PNG");
     private ImageIcon imgbtn = new ImageIcon("images/ok.PNG");
@@ -24,13 +34,20 @@ public class LogInGUI extends JFrame {
     private JLabel lblGame = new JLabel(welcomeLogo);
 
     private JLabel lblUsername = new JLabel("Anv√§ndarnamn");
+    private JLabel lblPassword = new JLabel("Lˆsenord");
+    private JLabel lblCreateUser = new JLabel("Create new user");
+
+
     private JLabel lblPi = new JLabel(piLogo);
 
-    private JTextField txtUsername = new JTextField("Anv√§ndarnamn (3-10 tecken)");
+    private JTextField txtUsername = new JTextField("Anv‰ndarnamn (3-10 tecken)");
+    private JTextField txtPassword = new JTextField();
+
 
     private Font myFont = new Font("Serif", Font.ITALIC | Font.BOLD, 20);
 
     private JButton btnLogin = new JButton(imgbtn);
+    private JButton btnCreate = new JButton();
 
     /**
      * Constructor that initializes the different components in the class
@@ -50,20 +67,31 @@ public class LogInGUI extends JFrame {
 
         lblGame.setBounds(40, 20, 300, 50);
         lblUsername.setBounds(50, 60, 100, 100);
+        lblPassword.setBounds(50, 80, 100, 100);
         txtUsername.setBounds(120, 100, 165, 25);
+        txtPassword.setBounds(120, 120, 165, 25);
         btnLogin.setBounds(168, 150, 59, 38);
+        
+        lblCreateUser.setBounds(50, 150, 50, 50);
+        btnCreate.setBounds(50, 150, 50, 50);
+        
         lblPi.setBounds(128, 145, 33, 40);
 
         lblPi.setVisible(false);
-
-        lblUsername.setText(name);
+        
+        lblCreateUser.setText("Create new user");
+        lblUsername.setText("Username");
+        lblPassword.setText("Password");
 
         txtUsername.setForeground(Color.GRAY);
 
         pnlMain.add(lblGame);
         pnlMain.add(lblUsername);
+        pnlMain.add(lblPassword);
         pnlMain.add(txtUsername);
+        pnlMain.add(txtPassword);
         pnlMain.add(btnLogin);
+        pnlMain.add(btnCreate);
         pnlMain.add(lblPi);
 
         btnLogin.setBorder(BorderFactory.createLineBorder(Color.WHITE));
@@ -77,7 +105,10 @@ public class LogInGUI extends JFrame {
     private void listeners() {
         txtUsername.addKeyListener(new LimitUsername());
         btnLogin.addActionListener(new Listener());
+        btnCreate.addActionListener(new createListener());
         txtUsername.addFocusListener(new Focus());
+    	
+    	
         btnLogin.addMouseListener(new MouseSubmit());
 
     }
@@ -158,16 +189,59 @@ public class LogInGUI extends JFrame {
      */
     private class Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+        	String txtuserName = txtUsername.getText();
+        	String txtpassWord = txtPassword.getText();
             if ((txtUsername.getText().trim().length() <= 10) && (txtUsername.getText().trim().length() >= 3)) {
-
-                controller.createUser();
-                setVisible(false);
-                dispose();
-            } else {
-                JOptionPane.showMessageDialog(null, "Ogiltigt anv√§ndarnamn");
-            }
+            	try {
+            		Connection connection = (Connection) DriverManager.getConnection(url, user, password);
+            		
+            		PreparedStatement st = (PreparedStatement) connection
+                            .prepareStatement("Select name, password from user where name=? and password=?");
+            		
+            		st.setString(1, txtuserName);
+                    st.setString(2, txtpassWord);
+                    ResultSet rs = st.executeQuery();
+                    
+                    if(rs.next()) {
+                    	controller.createUser();
+                        setVisible(false);
+                    	dispose();
+                        JOptionPane.showMessageDialog(btnLogin, "You have successfully logged in");
+                    }else {
+                        JOptionPane.showMessageDialog(btnLogin, "Wrong Username & Password");
+                    }
+            	}catch ( SQLException sqlException) {
+            		sqlException.printStackTrace();
+            	}
+            	
+            } 
         }
     }
+    
+    private class createListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String txtuserName = txtUsername.getText();
+        	String txtpassWord = txtPassword.getText();
+			try {
+        		Connection connection = (Connection) DriverManager.getConnection(url, user, password);
+        		
+        		String query = " insert into user (name , password)"
+        				+ " values (?,?)";
+        		
+        	      PreparedStatement st = connection.prepareStatement(query);
+        	      st.setString(1, txtuserName);
+        	      st.setString(2, txtpassWord);
+        	      
+        	      st.execute();
+        	      connection.close();
+
+        		
+			}catch ( SQLException sqlException) {
+        		sqlException.printStackTrace();
+        	}
+		}
+    }
+    
 
     public JTextField getTxtUsername() {
         return txtUsername;
